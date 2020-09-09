1e3fac97adcfb6a120938cfd009fabf5adcb472c
Jenna Koslowski <jenna.koslowski@yahoo.com>
2020-09-09 15:51:25 -0400

    add buttons for each move

--- src/index.js
+++ src/index.js
@@ -75,6 +75,20 @@ function Square(props){
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 'Go to move # ' + move : 'Go to game start';
+      
+      
+      return (
+        <li key = {move}>
+          <button onClick={() => this.jumpTo(move)}>
+            {desc}
+          </button>
+        </li>
+      );
+    });
+
       let status;
 
       if(winner){
@@ -84,7 +98,6 @@ function Square(props){
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
       }
 
-      
 
       return (
         <div className="game">
@@ -96,7 +109,7 @@ function Square(props){
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
